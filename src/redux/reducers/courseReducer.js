623de2c9dea4a08bcf43ce0109564bde43267c93
user <user@users-MacBook-Pro.local>
2022-04-21 13:53:23 -0500

    Adding action types folder

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,9 @@
+import * as types from "../actions/actionTypes";
+
 export default function courseReducer(state = [], action) {
   //Initializying state to an empty array because this will end up storying and array of courses
   switch (action.type) {
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       return [...state, { ...action.course }]; //This will update our Redux sotre by adddint the new action passed in the action.course
     default:
       return state; //If some other action is dispatched that thisparticular reducer doesn't care about, it shoul just return the unchanged state

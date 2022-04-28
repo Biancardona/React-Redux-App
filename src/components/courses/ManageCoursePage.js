503ba86de9c947d48d1d4d3918e1ce9644d672e1
user <user@users-MacBook-Pro.local>
2022-04-28 12:17:30 -0500

    Convert Class Component to Functional Component with hooks (ManageCoursePage)

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,24 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-// const ManageCoursePage = () => (
-//   <div>
-//     <h2>Courses</h2>
-//     <p>React, Redux and React Router for ultra-responsive web apps.</p>
-//   </div>
-// );
-
-// export default ManageCoursePage;
-
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
-
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (courses.length === 0) {
-     loadCourses().catch((error) => {
+      loadCourses().catch((error) => {
         alert("Loading courses fail" + error);
       });
     }
@@ -27,22 +16,21 @@ class ManageCoursePage extends React.Component {
         alert("Loading authors fail" + error);
       });
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  }, []);
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 //This is to clarified that we expect dispatch to be passed into de ManageCoursePage component and it will be passed in because connect automatically passes dispatch in if we omit that second argunment
 ManageCoursePage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  loadAuthors: PropTypes.func.isRequired, 
-  loadCourses: PropTypes.func.isRequired, 
+  loadAuthors: PropTypes.func.isRequired, //Only the actions we declared in mapDispatchToProps are passed in
+  loadCourses: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -54,13 +42,11 @@ function mapStateToProps(state) {
     authors: state.authors,
   };
 }
-
-//Changing mapDispatchtoProps to a single object
 const mapDispatchToProps = {
-      loadCourses: courseActions.loadCourses, 
-      loadAuthors: authorActions.loadAuthors, 
-    },
-
+  //list of courses available on this.props.courses
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage); //Connect function connects our components to REdux, takes 2 parameters. Then we take the result of this and call ManageCoursePage
 //When we omit mapDispatchProps, our component gets a dispatch prop injected automatically.
